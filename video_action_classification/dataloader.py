5b03b6aa09e69e0559d451df0563157905f2ac6b
KaiYin77 <kevin880208@gmail.com>
2022-10-01 23:18:43 +0800

    fix pooling size

--- video_action_classification/dataloader.py
+++ video_action_classification/dataloader.py
@@ -14,8 +14,9 @@ from pathlib import Path
 import sys
 
 class VideoActionDataset(Dataset):
-    def __init__(self, raw_dir):
+    def __init__(self, raw_dir, processed_dir):
         self.raw_dir = raw_dir
+        self.processed_dir = processed_dir
         self.all_video_list = sorted(self.raw_dir.rglob("*.mp4"))
         self.max_len = 82
         self.height = 90
@@ -26,30 +27,35 @@ class VideoActionDataset(Dataset):
         return len(self.all_video_list)
 
     def __getitem__(self, idx):
-        video_path = str(self.all_video_list[idx])
-        cap = cv2.VideoCapture(video_path)
-        if cap.isOpened():
-            success = True
-        else:
-            sucess = False
-            print('loading .mp4 error...')
+        sample_path = os.path.join(self.processed_dir, f'data_{idx}.pt')
+        try:
+            sample = torch.load(sample_path)
+        except:
+            video_path = str(self.all_video_list[idx])
+            cap = cv2.VideoCapture(video_path)
+            if cap.isOpened():
+                success = True
+            else:
+                sucess = False
+                print('loading .mp4 error...')
 
-        frame_list = torch.zeros(self.max_len, self.height, self.width, self.channel)
-        frame_count = 0
-        while(success):
-            success, frame = cap.read()
-            if success is False:
-                break
-            frame = self.crop_center_square(frame)
-            frame = cv2.resize(frame, (self.height, self.width))
-            frame_list[frame_count] = torch.from_numpy(frame)
-            frame_count += 1
+            frame_list = torch.zeros(self.max_len, self.height, self.width, self.channel)
+            frame_count = 0
+            while(success):
+                success, frame = cap.read()
+                if success is False:
+                    break
+                frame = self.crop_center_square(frame)
+                frame = cv2.resize(frame, (self.height, self.width))
+                frame_list[frame_count] = torch.from_numpy(frame)
+                frame_count += 1
 
-        sample = {}
-        sample['video'] = frame_list
-        parent_path = os.path.dirname(self.all_video_list[idx])
-        parent_dir = os.path.split(parent_path)[1]
-        sample['label'] = int(parent_dir)
+            sample = {}
+            sample['video'] = torch.permute(frame_list, (3, 0, 1, 2))
+            parent_path = os.path.dirname(self.all_video_list[idx])
+            parent_dir = os.path.split(parent_path)[1]
+            sample['label'] = torch.as_tensor(int(parent_dir))
+            torch.save(sample, sample_path)
         return sample
 
     def crop_center_square(self, frame):
@@ -63,14 +69,18 @@ if __name__ == '__main__':
     '''
     Unit test
     '''
-    debug = False
+    debug = False 
     batch_size = 2
     train_dir = Path('../data/hw1/train/')
-    dataset = VideoActionDataset(train_dir) 
+    processed_dir= Path('../data/hw1/processed/')
+    dataset = VideoActionDataset(train_dir, processed_dir) 
     dataloader = DataLoader(dataset, batch_size=batch_size)
     dataiter = tqdm(dataloader)
     for i, data in enumerate(dataiter):
         if debug:
             print('video.shape: ', data['video'].shape)
-            print('label: ', data['label'])
-        assert data['video'].shape == torch.Size([batch_size, 82, 90, 90, 3]), 'Frame shape should be (batch_size, 82, 90, 90, 3)'
+            print('label: ', data['label'].shape)
+        #(B, C, T, H, W)
+        assert data['video'].shape == torch.Size([batch_size, 3, 82, 90, 90]), 'Video shape should be (batch_size, 3, 82, 90, 90)'
+        #(B)
+        assert data['label'].shape == torch.Size([batch_size]), 'Label shape should be (batch_size)'
--- video_action_classification/train.py
+++ video_action_classification/train.py
@@ -1,12 +1,12 @@
 '''
 Boosted by lightning module
 '''
-
+import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
-from torch.utils.data import random_split
+from torch.utils.data.sampler import SubsetRandomSampler
 import pytorchvideo.models.resnet
 import pytorch_lightning as pl
 from dataloader import VideoActionDataset
@@ -14,8 +14,12 @@ from pathlib import Path
 import os 
 
 train_dir = Path('../data/hw1/train/')
+processed_dir = Path('../data/hw1/processed/')
 test_dir = Path('../data/hw1/test/')
-BATCHSIZE = 1
+ckpt_dir = Path('./weights/')
+BATCHSIZE = 8
+
+os.makedirs(processed_dir, exist_ok=True)
 
 class VideoActionClassifier(pl.LightningModule):
     def __init__(self):
@@ -29,24 +33,55 @@ class VideoActionClassifier(pl.LightningModule):
                   model_num_class=39,
                   norm=nn.BatchNorm3d,
                   activation=nn.ReLU,
+                  head_pool_kernel_size=(4,3,3),
               )
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-1)
         return optimizer
     
     def training_step(self, train_batch, batch_idx):
         y_hat = self.model(train_batch["video"])
         loss = F.cross_entropy(y_hat, train_batch["label"])
+        self.log("train_loss", loss.item())
         return loss
     
+    def validation_step(self, val_batch, val_idx):
+        y_hat = self.model(val_batch["video"])
+        loss = F.cross_entropy(y_hat, val_batch["label"])
+        self.log("val_loss", loss.item())
+
     def prepare_data(self):
-        self.dataset = VideoActionDataset(train_dir) 
-    
+        self.dataset = VideoActionDataset(train_dir, processed_dir)
+        
+        val_split = 0.2
+        random_seed = 1234
+        dataset_size = len(self.dataset)
+        indices = list(range(dataset_size))
+
+        split = int(np.floor(val_split * dataset_size))
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+        self.train_indices, self.val_indices = indices[split:], indices[:split]
+
     def train_dataloader(self):
-        return DataLoader(self.dataset, batch_size=BATCHSIZE)
+        train_sampler = SubsetRandomSampler(self.train_indices)
+        return DataLoader(
+                self.dataset, 
+                batch_size=BATCHSIZE, 
+                sampler=train_sampler,
+                num_workers=8
+                )
+    def val_dataloader(self):
+        val_sampler = SubsetRandomSampler(self.val_indices)
+        return DataLoader(
+                self.dataset, 
+                batch_size=1, 
+                sampler=val_sampler,
+                num_workers=8
+                )
     
 if __name__ == '__main__':
     model = VideoActionClassifier()
-    trainer = pl.Trainer()
+    trainer = pl.Trainer(default_root_dir=ckpt_dir)
     trainer.fit(model)
