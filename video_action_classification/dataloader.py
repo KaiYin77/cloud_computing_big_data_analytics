56f9faf123eeaada1636404290c61d429bfbbe2f
KaiYin77 <kevin880208@gmail.com>
2022-10-02 15:30:26 +0800

    remove torch save

--- video_action_classification/dataloader.py
+++ video_action_classification/dataloader.py
@@ -14,9 +14,8 @@ from pathlib import Path
 import sys
 
 class VideoActionDataset(Dataset):
-    def __init__(self, raw_dir, processed_dir):
+    def __init__(self, raw_dir):
         self.raw_dir = raw_dir
-        self.processed_dir = processed_dir
         self.all_video_list = sorted(self.raw_dir.rglob("*.mp4"))
         self.max_len = 82
         self.height = 90
@@ -27,36 +26,32 @@ class VideoActionDataset(Dataset):
         return len(self.all_video_list)
 
     def __getitem__(self, idx):
-        sample_path = os.path.join(self.processed_dir, f'data_{idx}.pt')
-        try:
-            sample = torch.load(sample_path)
-        except:
-            video_path = str(self.all_video_list[idx])
-            cap = cv2.VideoCapture(video_path)
-            if cap.isOpened():
-                success = True
-            else:
-                sucess = False
-                print('loading .mp4 error...')
+        video_path = str(self.all_video_list[idx])
+        cap = cv2.VideoCapture(video_path)
+        if cap.isOpened():
+            success = True
+        else:
+            sucess = False
+            print('loading .mp4 error...')
 
-            frame_list = torch.zeros(self.max_len, self.height, self.width, self.channel)
-            frame_count = 0
-            while(success):
-                success, frame = cap.read()
-                if success is False:
-                    break
-                frame = self.crop_center_square(frame)
-                frame = cv2.resize(frame, (self.height, self.width))
-                frame_list[frame_count] = torch.from_numpy(frame)
-                frame_count += 1
+        frame_list = torch.zeros(self.max_len, self.height, self.width, self.channel)
+        frame_count = 0
+        while(success):
+            success, frame = cap.read()
+            if success is False:
+                break
+            frame = self.crop_center_square(frame)
+            frame = cv2.resize(frame, (self.height, self.width))
+            frame_list[frame_count] = torch.from_numpy(frame)
+            frame_count += 1
 
-            sample = {}
-            frame_list = frame_list[::4] # downsample to 1/4 frame rate
-            sample['video'] = torch.permute(frame_list, (3, 0, 1, 2))
-            parent_path = os.path.dirname(self.all_video_list[idx])
-            parent_dir = os.path.split(parent_path)[1]
-            sample['label'] = torch.as_tensor(int(parent_dir))
-            #torch.save(sample, sample_path)
+        sample = {}
+        frame_list = frame_list[::4] # downsample to 1/4 frame rate
+        sample['video'] = torch.permute(frame_list, (3, 0, 1, 2))
+        parent_path = os.path.dirname(self.all_video_list[idx])
+        parent_dir = os.path.split(parent_path)[1]
+        sample['label'] = torch.as_tensor(int(parent_dir))
+
         return sample
 
     def crop_center_square(self, frame):
@@ -67,9 +62,8 @@ class VideoActionDataset(Dataset):
         return frame[start_y:start_y+min_dim,start_x:start_x+min_dim]
 
 class VideoActionTestDataset(Dataset):
-    def __init__(self, raw_dir, processed_dir):
+    def __init__(self, raw_dir):
         self.raw_dir = raw_dir
-        self.processed_dir = processed_dir
         self.all_video_list = sorted(self.raw_dir.rglob("*.mp4"))
         self.max_len = 82
         self.height = 90
@@ -80,33 +74,30 @@ class VideoActionTestDataset(Dataset):
         return len(self.all_video_list)
 
     def __getitem__(self, idx):
-        sample_path = os.path.join(self.processed_dir, f'data_{idx}.pt')
-        try:
-            sample = torch.load(sample_path)
-        except:
-            video_path = str(self.all_video_list[idx])
-            cap = cv2.VideoCapture(video_path)
-            if cap.isOpened():
-                success = True
-            else:
-                sucess = False
-                print('loading .mp4 error...')
+        video_path = str(self.all_video_list[idx])
+        cap = cv2.VideoCapture(video_path)
+        if cap.isOpened():
+            success = True
+        else:
+            sucess = False
+            print('loading .mp4 error...')
 
-            frame_list = torch.zeros(self.max_len, self.height, self.width, self.channel)
-            frame_count = 0
-            while(success):
-                success, frame = cap.read()
-                if success is False:
-                    break
-                frame = self.crop_center_square(frame)
-                frame = cv2.resize(frame, (self.height, self.width))
-                frame_list[frame_count] = torch.from_numpy(frame)
-                frame_count += 1
+        frame_list = torch.zeros(self.max_len, self.height, self.width, self.channel)
+        frame_count = 0
+        while(success):
+            success, frame = cap.read()
+            if success is False:
+                break
+            frame = self.crop_center_square(frame)
+            frame = cv2.resize(frame, (self.height, self.width))
+            frame_list[frame_count] = torch.from_numpy(frame)
+            frame_count += 1
 
-            sample = {}
-            frame_list = frame_list[::4] # downsample to 1/4 frame rate
-            sample['video'] = torch.permute(frame_list, (3, 0, 1, 2))
-            sample['video_name'] = os.path.basename(video_path)
+        sample = {}
+        frame_list = frame_list[::4] # downsample to 1/4 frame rate
+        sample['video'] = torch.permute(frame_list, (3, 0, 1, 2))
+        sample['video_name'] = os.path.basename(video_path)
+
         return sample
 
     def crop_center_square(self, frame):
@@ -123,8 +114,7 @@ if __name__ == '__main__':
     debug = False 
     batch_size = 2
     train_dir = Path('../data/hw1/train/')
-    processed_dir= Path('../data/hw1/processed/')
-    dataset = VideoActionDataset(train_dir, processed_dir) 
+    dataset = VideoActionDataset(train_dir) 
     dataloader = DataLoader(dataset, batch_size=batch_size)
     dataiter = tqdm(dataloader)
     for i, data in enumerate(dataiter):
@@ -132,6 +122,6 @@ if __name__ == '__main__':
             print('video.shape: ', data['video'].shape)
             print('label: ', data['label'].shape)
         #(B, C, T, H, W)
-        assert data['video'].shape == torch.Size([batch_size, 3, 82, 90, 90]), 'Video shape should be (batch_size, 3, 82, 90, 90)'
+        assert data['video'].shape == torch.Size([batch_size, 3, 21, 90, 90]), 'Video shape should be (batch_size, 3, 82, 90, 90)'
         #(B)
         assert data['label'].shape == torch.Size([batch_size]), 'Label shape should be (batch_size)'
