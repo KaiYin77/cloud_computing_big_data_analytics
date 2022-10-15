d69a733825e178631176813f16bcbed3999170e6
altonfang <a6601zxf@gmail.com>
2022-10-15 23:22:56 +0800

    modify lstm & add warmup optimizer

--- video_action_classification/train.py
+++ video_action_classification/train.py
@@ -89,10 +89,21 @@ class VideoActionClassifier(pl.LightningModule):
                 )
 
     def configure_optimizers(self):
+        self.warmup_epoch = 3
         self.optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
-        self.lr_scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.1)
+        self.lr_scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.5)
         return [self.optimizer], [self.lr_scheduler]
-    
+
+    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx, optimizer_closure, on_tpu=False, using_native_amp=False, using_lbfgs=False):
+        # update params
+        optimizer.step(closure=optimizer_closure)
+
+        # skip the first epochs
+        if (self.trainer.global_step < self.warmup_epoch * self.trainer.num_training_batches):
+            lr_scale = min(1.0, float(self.trainer.global_step + 1) / float(self.warmup_epoch * self.trainer.num_training_batches))
+            for pg in optimizer.param_groups:
+                pg["lr"] = lr_scale * self.trainer.lr_scheduler_configs[0].scheduler._get_closed_form_lr()[0]
+
     def training_step(self, train_batch, batch_idx):
         y_hat = self.model(train_batch["video"])
         loss = F.cross_entropy(y_hat, train_batch["label"])
