a0cd3c9037d4a9929be7775a112c0abbd9d1ba29
altonfang <a6601zxf@gmail.com>
2022-10-15 23:22:56 +0800

    modify lstm & add warmup optimizer

--- video_action_classification/models/vgg_lstm.py
+++ video_action_classification/models/vgg_lstm.py
@@ -10,35 +10,38 @@ import ipdb
 class VGGLSTM(pl.LightningModule):
     def __init__(self, num_classes=39):
         super(VGGLSTM, self).__init__()
-        self.vgg = vgg16(pretrained=False, num_classes=num_classes, dropout=0).features[:28]
-        self.lstm = nn.LSTM(input_size=25088, hidden_size=30, num_layers=1)
+        self.vgg = vgg16(pretrained=False, num_classes=num_classes, dropout=0).features
+        self.lstm = nn.LSTM(input_size=4608, hidden_size=256, num_layers=1)
         self.mlp = nn.Sequential(
-                  nn.Linear(840, 128),
+                  nn.Linear(256*28, 256),
                   nn.ReLU(),
-                  nn.Linear(128, 128),
+                  nn.Linear(256, 256),
                   nn.ReLU(),
-                  nn.Linear(128, 128),
+                  nn.Linear(256, 256),
                   nn.ReLU(),
-                  nn.Linear(128, num_classes),
+                  nn.Linear(256, num_classes),
         )
 
     def init_hidden(self, batch_size):
         return(
-          torch.randn(1, batch_size, 30).to(self.device),
-          torch.randn(1, batch_size, 30).to(self.device)
+          torch.randn(1, batch_size, 256).to(self.device),
+          torch.randn(1, batch_size, 256).to(self.device)
         )
 
     def forward(self, x_3d):
-        batch_size = x_3d.shape[0]
-        hidden = self.init_hidden(batch_size) 
+        batch_size, timestamp = x_3d.shape[0], x_3d.shape[1]
+        
         outputs = []
-        for t in range(x_3d.shape[1]):
-            x = self.vgg(x_3d[:, t, :, :, :])
-            x = x.reshape(1, x_3d.shape[0], -1)
-            x, hidden = self.lstm(x, hidden)
-            outputs.append(x)
-
-        outputs = torch.stack(outputs, dim=-1)
+        for t in range(timestamp):
+          output = self.vgg(x_3d[:, t, :, :, :])
+          outputs.append(output)
+        outputs = torch.stack(outputs)
+        
+        outputs = outputs.reshape(timestamp, batch_size, -1) 
+        hidden = self.init_hidden(batch_size) 
+        outputs, hidden = self.lstm(outputs, hidden)
+        
+        outputs = outputs.permute(1, 0, 2) 
        	outputs = outputs.reshape(batch_size, -1) 
         outputs = self.mlp(outputs)
 
