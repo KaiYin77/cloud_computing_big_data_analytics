0a61ffcd561864fa4642150368838a45e1d48839
KaiYin77 <kevin880208@gmail.com>
2022-12-02 20:45:45 +0800

    fix package missing

--- diffusion_generative_model/get_fid_score.py
+++ diffusion_generative_model/get_fid_score.py
@@ -1,8 +1,13 @@
-from pytorch_gan_metrics import get_fid
+import os
+from tqdm import tqdm
+
+import torch
 from torchvision.io import read_image
 
+from pytorch_gan_metrics import get_fid
+
 images = []
-for i in range(num_images):
+for i in tqdm(range(1, 10001)):
     path = os.path.join(f'./samples/{i:05d}.png')
     image = read_image(path) / 255.
     images.append(image)
